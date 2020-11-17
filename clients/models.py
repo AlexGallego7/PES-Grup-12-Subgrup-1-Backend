4060a244d4286bf88499456ed083081d973e6d0b
marc.soler.colomer <marc.soler.colomer@upc.edu>
2020-11-17 20:21:58 +0100

    age can be null

--- clients/models.py
+++ clients/models.py
@@ -4,5 +4,5 @@ from mongoengine import Document, fields
 class Clients(Document):
     _id = fields.IntField(primary_key=True, disabled=False)
     username = fields.StringField(required=True)
-    age = fields.IntField(required=False, min_value=14)
+    age = fields.IntField(required=False, null=True, blank=True)
     country = fields.StringField(required=False, null=True, blank=True)
