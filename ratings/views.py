838a236d14d989921007d0985d8917b438252921
marc.soler.colomer <marc.soler.colomer@upc.edu>
2021-01-02 16:00:41 +0100

    put ratings

--- ratings/views.py
+++ ratings/views.py
@@ -19,7 +19,7 @@ class RatingsView(APIView):
 
     @staticmethod
     def post(request):
-        request.data['_id'] = "Review_" + request.user.username + "_" + request.data['id_event']
+        request.data['_id'] = request.user.username + "_" + request.data['id_event']
         serializer = RatingsSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         if serializer.save():
@@ -48,3 +48,22 @@ class RatingView(APIView):
         serializer = RatingsSerializer(Ratings.objects.filter(_id=self.kwargs['id']), many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        rating_id = self.kwargs['id']
+        try:
+            rating = Ratings.objects.get(_id=rating_id)
+        except Events.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        number_of_ratings = len(RatingsSerializer(Ratings.objects.filter(id_event=rating.id_event), many=True).data)
+        event = EventsSerializer(Events.objects.get(_id=rating.id_event)).data.copy()
+        event['ratings'] -= rating.rate
+        event['ratings'] = (event['ratings'] + request.data["rate"]) / number_of_ratings
+        eserializer = EventsSerializer(data=event)
+        eserializer.is_valid(raise_exception=True)
+        eserializer.save()
+        serializer = RatingsSerializer(rating, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_200_OK)
