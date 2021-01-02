5f8f1892086e33a46db322689ae8aa7b6e954eee
marc.soler.colomer <marc.soler.colomer@upc.edu>
2021-01-02 12:59:09 +0100

    add logo field

--- events/models.py
+++ events/models.py
@@ -5,7 +5,7 @@ from mongoengine import Document, fields
 class Events(Document):
     _id = fields.StringField(primary_key=True, disabled=False)
     name = fields.StringField(required=True, max_length=50)
-    # logo = fields.ImageField(required=False)
+    logo = fields.URLField(required=False)
     date = fields.StringField(required=True)
     hourIni = fields.StringField(required=True)
     hourEnd = fields.StringField(required=True)
